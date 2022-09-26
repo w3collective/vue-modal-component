80c4fcd7994a10a87339ae3d979e655d6e9425c5
W3Collective <contact@w3collective.com>
2022-09-26 14:42:10 +1000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <button @click="openModal">Open</button>
+    <DemoModal v-show="isModalVisible" @close="closeModal" />
+  </div>
+</template>
+
+<script>
+  import DemoModal from './components/DemoModal.vue';
+  export default {
+    name: 'App',
+    components: {
+      DemoModal,
+    },
+    data() {
+      return {
+        isModalVisible: false,
+      };
+    },
+    methods: {
+      openModal() {
+        this.isModalVisible = true;
+      },
+      closeModal() {
+        this.isModalVisible = false;
+      }
+    }
+  };
+</script>
---
+++ src/components/DemoModal.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="modal">
+    <div class="modal-inner">      
+      <button @click="close">&times;</button>
+      <p>Hello World!</p>      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {  
+  methods: {
+    close() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style>
+.modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: lightseagreen;
+}
+.modal-inner {  
+  background-color: white;
+  border-radius: 0.5em;
+  padding: 2em;
+  margin: auto;
+}
+</style>
